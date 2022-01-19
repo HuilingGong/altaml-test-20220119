04af8a33966e1bd5387a4c593cf6b975c2f6f2ac
saharicodes <sahar.sedaghati@gmail.com>
2022-01-19 10:47:07 -0700

    feat: add docstring to addition function

--- main.py
+++ main.py
@@ -2,5 +2,13 @@ def multiply(num1, num2):
 		return num1 * num2
 
 def add(num1, num2):
-    
+    """Addition
+
+    Arguments:
+        num1 {[type]} -- [description]
+        num2 {[type]} -- [description]
+
+    Returns:
+        [type] -- [description]
+    """
 	return num1 + num2
